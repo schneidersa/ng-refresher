eb6ddfed71f053a2376036a7435819bcf10c1dbc
Sandra Schneider <san.schneider@ostfalia.de>
2020-01-11 23:02:11 +0100

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-refresher';
+
+  persons: string[] = ['Peter', 'Paul', 'Anna'];
+
+  onPersonCreated(name: string) {
+    this.persons.push(name);
+  }
 }
