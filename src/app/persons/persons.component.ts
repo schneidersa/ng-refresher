30e0b7d860cd99242d20c3190583b4bb876eda62
Sandra Schneider <san.schneider@ostfalia.de>
2020-01-12 11:50:47 +0100

    Working with Angular Lifecycle Hooks
    + add OnInit-Hook to PersonsComponent
    + make service accessible
    + link to angular documentation 
    https://angular.io/guide/lifecycle-hooks

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PersonsService } from './persons.service';
 
 @Component({
   selector: 'app-persons',
   templateUrl: './persons.component.html'
 })
-export class PersonsComponent {
+export class PersonsComponent implements OnInit {             // https://angular.io/guide/lifecycle-hooks
   personList: string[];
+  // private personsService: PersonsService;
 
-  constructor(personsService: PersonsService) {
-    this.personList = personsService.persons;
+  constructor(private prsService: PersonsService) {
+    // this.personList = personsService.persons;
+    // this.personsService = prsService;
+  }
+
+  ngOnInit() {
+    this.personList = this.prsService.persons;
   }
 }
