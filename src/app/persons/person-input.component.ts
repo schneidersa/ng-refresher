c50506a8cc6f3827023bed57bdebbc92c3211a25
Sandra Schneider <san.schneider@ostfalia.de>
2020-01-12 11:58:40 +0100

    Adding a Person with Services
    + use service in person-input.component

--- src/app/persons/person-input.component.ts
+++ src/app/persons/person-input.component.ts
@@ -1,4 +1,5 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
+import { PersonsService } from './persons.service';
 
 @Component({
   selector: 'app-person-input',
@@ -6,12 +7,17 @@ import { Component, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./person-input-component.css']
 })
 export class PersonInputComponent {
-  @Output() personCreate = new EventEmitter<string>();              // passing data with custom events
   enteredPersonName = '';
+
+  constructor(private prsService: PersonsService) {
+
+  }
 
   onCreatePerson() {
     console.log('Created a person: ' + this.enteredPersonName);
-    this.personCreate.emit(this.enteredPersonName);
+    this.prsService.addPerson(this.enteredPersonName);
     this.enteredPersonName = '';
   }
+
+
 }
--- src/app/persons/persons.service.ts
+++ src/app/persons/persons.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root'      // service object is everywhere the same object!
 })
 export class PersonsService {
   persons: string[] = ['Peter', 'Paul', 'Anna'];
 
   addPerson(name: string) {
     this.persons.push(name);
+    console.log(this.persons);
   }
 }
