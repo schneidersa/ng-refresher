452001483d976c6834eef6434e991721ee550ebf
Sandra Schneider <san.schneider@ostfalia.de>
2020-01-14 12:01:27 +0100

    "Push"ing Data Around with Subjects
    + add an observer to personsChanged to notice changes on the list and push it to frontend
    + subscribe / unsubscribe to the list

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PersonsService } from './persons.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-persons',
   templateUrl: './persons.component.html'
 })
-export class PersonsComponent implements OnInit {             // https://angular.io/guide/lifecycle-hooks
+export class PersonsComponent implements OnInit, OnDestroy {                            // OnInit - https://angular.io/guide/lifecycle-hooks
   personList: string[];
+  private personListSubscribe: Subscription;
   // private personsService: PersonsService;
 
   constructor(private prsService: PersonsService) {
@@ -16,6 +18,14 @@ export class PersonsComponent implements OnInit {             // https://angular
 
   ngOnInit() {
     this.personList = this.prsService.persons;
+    this.personListSubscribe = this.prsService.personsChanged.subscribe(persons => {    // subscribe to list of personsChanged
+      this.personList = persons;
+    });
+  }
+
+  ngOnDestroy() {
+    // tslint:disable-next-line: max-line-length
+    this.personListSubscribe.unsubscribe();                                             // unsubscribe to list of personsChanged to prevent memory leaks
   }
 
   onRemovePerson(personName: string) {
--- src/app/persons/persons.service.ts
+++ src/app/persons/persons.service.ts
@@ -1,20 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'      // service object is everywhere the same object!
+  providedIn: 'root'                                          // service object is everywhere the same object!
 })
 export class PersonsService {
+  personsChanged = new Subject<string[]>();                   // Observable
   persons: string[] = ['Peter', 'Paul', 'Anna'];
 
   addPerson(name: string) {
     this.persons.push(name);
-    console.log(this.persons);
+    this.personsChanged.next(this.persons);                   // emitting events through personsChanged subject to update list of persons
   }
 
   removePerson(name: string) {
     this.persons = this.persons.filter(person => {
       return person !== name;
     });
-    console.log(this.persons);
+    this.personsChanged.next(this.persons);                   // emitting events through personsChanged subject to update list of persons
   }
 }
